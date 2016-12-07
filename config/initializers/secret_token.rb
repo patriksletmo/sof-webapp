563d630fd2141acc18b593f2bbc5a0857c6aa95f
Lukas Lindqvist <mail@lukaslindqvist.se>
2016-12-07 08:35:00 +0100

    Removed test-app code

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Testrailsapp::Application.config.secret_token = '356f28c8cef6dfdde235596b15b3ec0d2126b847a2c41aea281851d6d45e31d53502836175e8e4a8f1731ea53b010338f3063349fcd1e1ef858a9134889102c5'
