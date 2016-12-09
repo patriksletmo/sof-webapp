f5145688d7e2cf16c3e73452d142e879aacb3461
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-12-09 17:33:46 +0100

    Refactored navigation and moved it out to a new controller
    Calls to the database are now prepared for authentication and moved to a separate class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::Base
+  include Session
   protect_from_forgery with: :exception
-  before_action :load_menu_items
-  helper_method :menu_items
-
-  protected
-  def menu_items
-    @active_menu_items
-  end
-
-  def load_menu_items
-    @active_menu_items ||= HTTParty.get("#{Rails.configuration.database_api_url}/api/v1/menu")
-
-    status_code = @active_menu_items.code
-    unless status_code >= 200 and status_code <= 299
-      raise 'Unable to fetch menu items from database'
-    end
-  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,2 @@
-class PagesController < ApplicationController
+class PagesController < NavigationController
 end
