59ee9ec1a215aadb51d941d2a780181788dcb616
Jacob Johansson <jojje133@gmail.com>
2016-12-13 13:44:17 +0100

    rought layout for index, contacts and info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,20 @@
 class PagesController < ApplicationController
+  def index
+    @index = "index"
+    @info = "info"
+    @contacts = "contacts"
+
+  end
+
+  def info
+    @index = "index"
+    @info = "info"
+    @contacts = "contacts"
+  end
+
+  def contacts
+    @index = "index"
+    @info = "info"
+    @contacts = "contacts"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 
+  get 'index' => 'pages#index'
+  get 'info' => 'pages#info'
+  get 'contacts' => 'pages#contacts'
 end
