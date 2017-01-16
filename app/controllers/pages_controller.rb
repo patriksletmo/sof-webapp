0a511e71b2bacf78f3e83dbd8654679e2c71a8ad
Jacob Johansson <jojje133@gmail.com>
2017-01-16 12:59:17 +0100

    page-title now looks good

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,17 @@
 class PagesController < ApplicationController
   def index
-
+    @header_title = "Studentorkesterfestivalen 2017"
+    @header = "SOF17"
   end
 
   def info
+    @header_title = "Info"
+    @header = "Information"
   end
 
   def contacts
+    @header_title = "Contacts"
+    @header = "Styrelsen"
     @contacts = Contact.all
 
 
