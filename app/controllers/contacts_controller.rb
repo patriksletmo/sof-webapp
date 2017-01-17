4c4303a8b6bc7c80f55ae740e7ea7427a617c3d5
Jacob Johansson <jojje133@gmail.com>
2017-01-17 13:21:27 +0100

    added footer and put the header as a variable @header-title

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,21 +2,25 @@ class ContactsController < ApplicationController
 
   def press
     @contacts = Contact.all
-
+    @header_title = "Press"
   end
 
   def voloteer
     @contacts = Contact.all
+    @header_title = "Volonteer"
 
   end
 
   def expo
     @contacts = Contact.all
 
+    @header_title = "Expo"
   end
 
   def contact
     @contacts = Contact.all
+
+    @header_title = "Board"
   end
 
 end
