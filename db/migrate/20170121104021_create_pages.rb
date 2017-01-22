3024917f43f05725a47b60d161540f93a0a91ea2
Jacob Johansson <jojje133@gmail.com>
2017-01-22 12:12:37 +0100

    fixed bug with dubble columns

--- db/migrate/20170121104021_create_pages.rb
+++ db/migrate/20170121104021_create_pages.rb
@@ -1,12 +1,6 @@
 class CreatePages < ActiveRecord::Migration[5.0]
   def change
     create_table :pages do |t|
-      t.string :category
-      t.string :page
-      t.string :header
-      t.text :content
-      t.boolean :show_in_menu
-
       t.timestamps
     end
   end
