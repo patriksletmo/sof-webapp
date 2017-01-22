916e08e7dfdf1bcf64388d1a0c2c7f7d97160497
Lukas Lindqvist <mail@lukaslindqvist.se>
2017-01-22 11:43:18 +0100

    Reformated colorcoding and fixed comment in _navigation.js

--- app/assets/javascripts/_navigation.js
+++ app/assets/javascripts/_navigation.js
@@ -1,13 +1,8 @@
-/**
- * Created by lukas on 2017-01-15.
- */
-
-
 document.addEventListener('turbolinks:load', function () {
     // Initialize collapse button
     $('.button-collapse').sideNav();
 
-    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    // Initialize collapsible
     $('.collapsible').collapsible();
 });
 
