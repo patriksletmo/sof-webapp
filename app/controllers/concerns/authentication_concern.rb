27e8680139a45740cbc83d2e4e0a216f3204b7bd
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-02-03 15:18:51 +0100

    Removed user model in favor of json response

--- app/controllers/concerns/authentication_concern.rb
+++ app/controllers/concerns/authentication_concern.rb
@@ -58,13 +58,13 @@ module AuthenticationConcern
 
   def current_user
     if has_token?
-      @current_user = User.from_response(database.current_user) unless instance_variable_defined? :@current_user
+      @current_user = database.current_user.body! unless instance_variable_defined? :@current_user
       @current_user
     end
   end
 
   def temporary_user
-    @temporary_user = User.from_response(temporary_database.current_user) unless instance_variable_defined? :@temporary_user
+    @temporary_user = temporary_database.current_user.body! unless instance_variable_defined? :@temporary_user
     @temporary_user
   end
 end
