01a890d7160996ae2c9ed80bb5324da283feda50
Jacob Johansson <jojje133@gmail.com>
2017-02-04 09:53:42 +0100

    fixed functionaliy to add Tshirts depending on gender and size

--- app/assets/javascripts/register.js
+++ app/assets/javascripts/register.js
@@ -2,6 +2,24 @@
  * Created by Jacob on 2017-01-28.
  */
 
+function togglePossibleTshirt(){
+    console.log("toggla vilka tröjor som kan köpas");
+
+    var tshirt = document.getElementById('select-selected-tshirt');
+    var gender = document.getElementById('switch-gender');
+
+    console.log();
+    // checked = Female
+    if(gender.checked){
+        tshirt[tshirt.length-1].disabled = true;
+    } else {
+        tshirt[tshirt.length-1].disabled = false;
+    }
+
+
+}
+
+
 function toggleSpecialkost(e) {
     var target = document.getElementById('field-specialfood');
     if (e.checked) {
@@ -15,41 +33,40 @@ function toggleSpecialkost(e) {
 function addTshirt() {
     var target = document.getElementById('collection-of-tshirts');
     var tshirt = document.getElementById('select-selected-tshirt');
-    console.log(target);
-    console.log(typeof(tshirt.value));
+    var gender = document.getElementById('switch-gender');
+
+
+
+    console.log(target.childNodes.length);
+    if(target.childNodes.length > 4){
+        console.log("too many Tshirts");
+        return;
+    }
+
+
+    var Tshirt_gender
+    if(gender.checked){
+        Tshirt_gender = "Female ";
+    } else {
+        Tshirt_gender = "Male "
+    }
+
+
 
     var fuckedup = false;;
     var entry =  document.createElement('li');
+    // add a delete icon to the right side
+
+    // remove t-shirt from list
     entry.onclick = function () {
         this.parentElement.removeChild(this);
     }
-    var size = "null";
+    var size = tshirt.value;
 
+    size = Tshirt_gender + size;
+    entry.appendChild(document.createTextNode(size));
+    target.appendChild(entry);
 
-
-    switch (tshirt.value){
-        case '1':
-            size = "XS";
-            break;
-        case '2':
-            size = "S";
-            break;
-        case '3':
-            size = "M";
-            break;
-        case '4':
-            size = "L";
-            break;
-        case '5':
-            size = "XL";
-            break;
-        default:
-            fuckedup = true;
-    }
-    if(!fuckedup) {
-        entry.appendChild(document.createTextNode(size));
-        target.appendChild(entry);
-    }
 }
 
 function togglePerformWithOther(e) {
