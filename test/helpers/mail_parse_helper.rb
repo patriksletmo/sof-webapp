b019d641104734b11ead653a58f911b14ffd384d
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-02-25 12:50:30 +0100

    Correctly handle argument for find_link_in(email, args)

--- test/helpers/mail_parse_helper.rb
+++ test/helpers/mail_parse_helper.rb
@@ -1,7 +1,7 @@
-def find_link_in(email, with_title)
+def find_link_in(email, args)
   content = Nokogiri::HTML(email)
   link_tags = content.css('a')
-  matching_tag = link_tags.find { |x| x.text == with_title }
+  matching_tag = link_tags.find { |x| x.text == args[:with_title] }
   unless matching_tag.nil?
     matching_tag['href']
   end
