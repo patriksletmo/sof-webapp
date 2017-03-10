83efa22b5e765aa9ab7fa4640a5dd87f203e130c
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-03-10 11:35:19 +0100

    Format CSV encoding correctly for Excel

--- app/controllers/orchestra_controller.rb
+++ app/controllers/orchestra_controller.rb
@@ -91,7 +91,7 @@ class OrchestraController < NavigationController
 
     respond_to do |format|
       format.html
-      format.csv { send_data @orchestraSignups }
+      format.csv { send_data @orchestraSignups.encode('iso-8859-1'), :filename => 'orkestersammanställning.csv', :type => 'text/csv; charset=iso-8859-1; header=present' }
     end
     
   end
