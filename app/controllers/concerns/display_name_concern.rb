ecfae25558610d99971d4a39c4a1fa6aff0f5890
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-03-25 13:00:33 +0100

    Make sure users can log out from the real name form

--- app/controllers/concerns/display_name_concern.rb
+++ app/controllers/concerns/display_name_concern.rb
@@ -4,11 +4,16 @@ module DisplayNameConcern
   NAG_CONTROLLER = 'user'
   NAG_ACTION = 'nag_display_name'
 
+  LOGOUT_CONTROLLER = 'user'
+  LOGOUT_ACTION = 'logout'
+
   included do
     before_action :ensure_user_has_display_name
   end
 
   def ensure_user_has_display_name
+    return if is_logout_path?
+
     unless current_user.nil?
       if has_display_name? and is_nag_path?
         redirect_to root_url
@@ -28,6 +33,10 @@ module DisplayNameConcern
 
   def is_nag_path?
     controller_name == NAG_CONTROLLER and action_name == NAG_ACTION
+  end
+
+  def is_logout_path?
+    controller_name == LOGOUT_CONTROLLER and action_name == LOGOUT_ACTION
   end
 
 end
