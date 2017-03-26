7fa72753417c7acfa330dac9c799a735c375be06
Lukas Lindqvist <mail@lukaslindqvist.se>
2017-03-26 18:13:26 +0200

    Finalized funkis category

--- app/controllers/funkis_controller.rb
+++ app/controllers/funkis_controller.rb
@@ -5,6 +5,19 @@ class FunkisController < NavigationController
 
   def categories
     @categories = database.funkis_categories
+
+    # Sort the funkis_shifts per day
+    @categories.each do |category|
+      days = Hash.new { |h, k| h[k] = [] }
+      category['available_shifts'].each do |shift|
+        if days.key?(shift['day'])
+          days[shift['day']] << shift
+        else
+          days[shift['day']] = [shift]
+        end
+      end
+      category['funkis_shifts'] = days
+    end
   end
 
   def application
