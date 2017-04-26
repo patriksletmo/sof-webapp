e70db956413569a7b8de6127789d75904c19ce45
Lukas Lindqvist <mail@lukaslindqvist.se>
2017-04-26 15:10:40 +0200

    Disabled medals, also contains new controller because im retard with git

---
+++ app/controllers/order_management_controller.rb
@@ -0,0 +1,2 @@
+class OrderManagementController < ApplicationController
+end
---
+++ test/controllers/order_management_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OrderManagementControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
