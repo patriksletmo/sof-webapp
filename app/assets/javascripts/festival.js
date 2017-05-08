88623e39830593cf410d206813c5c56a5c70abde
Lukas Lindqvist <mail@lukaslindqvist.se>
2017-05-08 12:03:35 +0200

    fixed artist-schedule to actually be nice

--- app/assets/javascripts/festival.js
+++ app/assets/javascripts/festival.js
@@ -1,11 +1,20 @@
 
 document.addEventListener('turbolinks:load', function () {
-    $('#schedule-tab-bar').tabs({
-        swipeable: true
-    });
-
     $('a[href*=\\#]').on('click', function (event) {
         event.preventDefault();
         $('html,body').animate({scrollTop: $(this.hash).offset().top}, 300);
     });
+
+    function setSchedule() {
+        var img_id = $(this).val();
+        $('.schedule-img').each(function () {
+            if (this.id === ('schedule-' + img_id)) {
+                $(this).css('display', 'inline-block');
+            } else {
+                $(this).hide();
+            }
+        });
+    }
+
+    document.getElementById("schedule-select").onchange = setSchedule;
 });
