8e8bfbde22e4bde54cb75fa383de90d3f534c022
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-05-10 09:57:15 +0200

    Redirect away from order statistics if permissions are missing

--- app/controllers/order_statistics_controller.rb
+++ app/controllers/order_statistics_controller.rb
@@ -1,8 +1,13 @@
 class OrderStatisticsController < NavigationController
   def index
+    return if require_login!
+
     date_from = '2017-05-01'
     date_to = Date.today.to_s
 
     @stats = database.order_stats_summary(date_from, date_to, params[:sum])
+    unless @stats.success?
+      redirect_to '/'
+    end
   end
 end
