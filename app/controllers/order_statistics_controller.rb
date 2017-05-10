3cee54f722cff228a623817c6c92cb1c8d17e1a4
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-05-10 16:34:23 +0200

    Correct date and more colors

--- app/controllers/order_statistics_controller.rb
+++ app/controllers/order_statistics_controller.rb
@@ -2,7 +2,7 @@ class OrderStatisticsController < NavigationController
   def index
     return if require_login!
 
-    date_from = '2017-05-01'
+    date_from = '2017-04-23'
     date_to = Date.today.to_s
 
     @stats = database.order_stats_summary(date_from, date_to, params[:sum])
