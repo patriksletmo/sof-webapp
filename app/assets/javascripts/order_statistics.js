c0ccc2a35a4b2ca4b3c17cc86c8124fe9130b5d2
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-05-12 01:42:16 +0200

    Improved statistics page

--- app/assets/javascripts/order_statistics.js
+++ app/assets/javascripts/order_statistics.js
@@ -3,28 +3,16 @@ var selectedStatsParam = null; // Will be overwritten if not null.
 $(document).on('turbolinks:load', function () {
     var sum = window.location.search.indexOf('sum') != -1;
 
-    $('#stat-params-select').change(function() {
-        var search = '?parameter=' + $(this).val();
-        if (sum) {
-            search += '&sum=true';
-        }
+    function loadNewPage() {
+        window.location.search = '?parameter=' + $('#stat-params-select').val() + '&sum=' + $('#sum-switch').prop('checked') + '&date=' + $('#set-date-field').val();
+    }
 
-        window.location.search = search;
-    });
+    $('#stat-params-select').change(loadNewPage);
+    $('#sum-switch').change(loadNewPage);
+    $('#set-date-field').change(loadNewPage);
 
-    $('#sum-switch').change(function() {
-        if ($(this).prop('checked')) {
-            if (selectedStatsParam != null) {
-                window.location.search = '?parameter=' + selectedStatsParam + '&sum=true';
-            } else {
-                window.location.search = '?sum=true'
-            }
-        } else {
-            if (selectedStatsParam != null) {
-                window.location.search = '?parameter=' + selectedStatsParam;
-            } else {
-                window.location.search = '';
-            }
-        }
+    $('#reset-date-field').click(function() {
+        $('#set-date-field').val(null);
+        loadNewPage();
     });
 });
--- app/controllers/order_statistics_controller.rb
+++ app/controllers/order_statistics_controller.rb
@@ -2,7 +2,7 @@ class OrderStatisticsController < NavigationController
   def index
     return if require_login!
 
-    @stats = database.order_stats_summary(params[:parameter], params[:sum])
+    @stats = database.order_stats_summary(params[:parameter], params[:sum], params[:date])
     unless @stats.success?
       redirect_to '/'
     end
